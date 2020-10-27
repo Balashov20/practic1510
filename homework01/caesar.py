0a8b8f376ffa6d5eb041adec5c7206c8ae3e88a9
Balashov20 <balashov895765@gmail.com>
2020-10-27 19:02:49 +0300

    Реализован шифр Цезаря

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -16,9 +16,9 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     for i in range(len(plaintext)):
-        if (ord(plaintext[i])>=88 and ord(plaintext[i])<=90) or (ord(plaintext[i])>=120 and ord(plaintext[i])<=122):
+        if (ord(plaintext[i])>=91-shift and ord(plaintext[i])<=90) or (ord(plaintext[i])>=123-shift and ord(plaintext[i])<=122):
             ciphertext+=chr(ord(plaintext[i])-26+shift)
-        elif (ord(plaintext[i])>=65 and ord(plaintext[i])<=87) or (ord(plaintext[i])>=97 and ord(plaintext[i])<=119):
+        elif (ord(plaintext[i])>=65 and ord(plaintext[i])<=90 - shift) or (ord(plaintext[i])>=97 and ord(plaintext[i])<=122 - shift):
             ciphertext+=chr(ord(plaintext[i])+shift)
         else:
             ciphertext += plaintext[i]
@@ -40,11 +40,11 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     for i in range(len(ciphertext)):
-        if (ord(ciphertext[i]) >= 65 and ord(ciphertext[i]) <= 67) or (
-                ord(ciphertext[i]) >= 97 and ord(ciphertext[i]) <= 99):
+        if (ord(ciphertext[i]) >= 65 and ord(ciphertext[i]) <= 64 + shift) or (
+                ord(ciphertext[i]) >= 97 and ord(ciphertext[i]) <= 96 + shift):
             plaintext += chr(ord(ciphertext[i]) + 26 - shift)
-        elif (ord(ciphertext[i]) >= 68 and ord(ciphertext[i]) <= 90) or (
-                ord(ciphertext[i]) >= 100 and ord(ciphertext[i]) <= 122):
+        elif (ord(ciphertext[i]) >= 65 + shift and ord(ciphertext[i]) <= 90) or (
+                ord(ciphertext[i]) >= 97 + shift and ord(ciphertext[i]) <= 122):
             plaintext += chr(ord(ciphertext[i]) - shift)
         else:
             plaintext += (ciphertext[i])
@@ -58,3 +58,5 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     best_shift = 0
     # PUT YOUR CODE HERE
     return best_shift
+
+print(encrypt_caesar('xyz', 4))
