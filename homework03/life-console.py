930d5ceeeb7e4ca09ff98504f2d364c6433581a6
Balashov20 <balashov895765@gmail.com>
2021-03-06 15:01:14 +0300

    console is done

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -8,40 +8,38 @@ from ui import UI
 
 class Console(UI):
 
-    def __init__(self, life: GameOfLife, speed: int=10) -> None:
+    def __init__(self, life: GameOfLife, speed: int = 10) -> None:
         super().__init__(life)
 
         self.speed = speed
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        rectangle(screen, 0, 0, self.life.rows + 1, self.life.cols + 1)
+        rectangle(screen, 0, 0, self.life.rows+1, self.life.cols+1)
         pass
 
     def draw_grid(self, screen) -> None:
-        """ Отобразить состояние клеток. """
         for i in range(self.life.rows):
             for j in range(self.life.cols):
                 if self.life.curr_generation[i][j] == 1:
-                    screen.addch(i + 1, j + 1, '*')
+                    screen.addch(i+1, j+1, '*')
                 else:
-                    screen.addch(i + 1, j + 1, ' ')
+                    screen.addch(i+1, j+1, ' ')
         pass
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
         running = True
         while running:
             self.draw_borders(screen)
             self.draw_grid(screen)
 
             if self.life.is_changing and self.life.is_max_generations_exceeded:
-                self.life.step()
+               self.life.step()
             else:
                 running = False
             screen.refresh()
-            time.sleep(1 / self.speed)
+            time.sleep(1/self.speed)
         curses.endwin()
         pass
 
